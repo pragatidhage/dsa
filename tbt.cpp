efcdd40ee643c27684f324c4e6594019d3d7ee6d
Pragati Dhage <pragatidhage971@gmail.com>
2023-03-04 19:34:50 +0530

    added tbt program

---
+++ tbt.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    bool lbit, rbit;
+
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+        rbit = lbit = 1;
+    }
+};
+class TBST
+{
+public:
+    Node *dummy;
+    Node *root;
+    TBST()
+    {
+        dummy = new Node(-1);
+        dummy->rbit = 0;
+        dummy->lbit = 1;
+        dummy->left = dummy;
+        dummy->right = dummy;
+    }
+    void insert(int data)
+    {
+        if (dummy->lbit == 1)
+        {
+            root = new Node(data);
+            root->left = dummy;
+            root->lbit = 1;
+            root->right = dummy;
+            root->rbit = 1;
+            dummy->left = root;
+            dummy->lbit = 0;
+            return;
+        }
+        else
+        {
+            Node *temp = root;
+            while (1)
+            {
+                if (data < temp->data)
+                {
+                    if (temp->lbit == 1)
+                    {
+                        Node *newNode = new Node(data);
+                        newNode->left = temp->left;
+                        newNode->lbit = 1;
+                        newNode->right = temp;
+                        newNode->rbit = 1;
+                        temp->left = newNode;
+                        temp->lbit = 0;
+                        return;
+                    }
+                    else
+                        temp = temp->left;
+                }
+                if(data>temp->data){
+                    if(temp->rbit==1){
+                        Node* newNode = new Node(data);
+                        newNode->right=temp->right;
+                        newNode->rbit=1;
+                        newNode->left=temp;
+                        newNode->lbit=1;
+                        temp->right=newNode;
+                        temp->rbit=0;
+                        return;
+                    }
+                    else
+                        temp=temp->right;
+                }
+            }
+        }
+    }
+    Node* inorderSuccessor(Node* current){
+        if(current->rbit==1)
+            return current->right;
+        else
+            current=current->right;
+        while (current->lbit==0){
+            current=current->left;
+        }
+        return current;
+        
+    }
+    void inorder(){
+        Node* current = root;
+        while (current->lbit==0)
+            current=current->left;
+        while (current!=dummy){
+            cout<<current->data<<" ";
+            current=inorderSuccessor(current);
+        } 
+    }
+    Node* preorderSuccessor(Node* current){
+        if(current->lbit==0)
+            return current->left;
+        while (current->rbit==1)
+            current=current->right;
+        return current->right;
+    }
+    void preorder(){
+        Node* current = root;
+        while (current!=dummy){
+            cout<<current->data<<" ";
+            current=preorderSuccessor(current);
+        }
+    }
+};
+int main()
+{
+    TBST tbst;
+    tbst.insert(5);
+    tbst.insert(3);
+    tbst.insert(7);
+    tbst.insert(8);
+    tbst.insert(9);
+    tbst.insert(11);
+    tbst.insert(4);
+    tbst.insert(2);
+    tbst.insert(6);
+    cout<<"The inorder traversal of TBST without recurssion is:"<<endl;
+    tbst.inorder();
+    cout<<endl<<endl;
+    cout<<"The preorder traversal of TBST wihtout recurssion is:"<<endl;
+    tbst.preorder();
+    cout<<endl<<endl;
+}
